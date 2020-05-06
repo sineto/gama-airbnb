f72924344ae287a4b41a3e0c234c2832fc71e893
Sinésio Neto <sn.logbackup@gmail.com>
2020-05-06 12:14:48 -0300

    feat(js): add api to build new data

---
+++ js/getData.js
@@ -0,0 +1,23 @@
+export const sheetyUrl = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72';
+export const citiesUrl = 'https://raw.githubusercontent.com/kelvins/Municipios-Brasileiros/master/json/municipios.json'; // municípios brasileiros
+
+export async function get(apiUrl) {
+	let response = await fetch(apiUrl);
+	return response.json();
+}
+
+export function rebaseData(sheety, cities) {
+	let data = [];
+	sheety.forEach(item => {
+		let position = Math.floor(Math.random() * cities.length);
+		let city = cities[position];
+
+		item['city_name'] = city.nome;
+		item['lat'] = city.latitude;
+		item['lon'] = city.longitude;
+
+		data.push(item);
+	});
+
+	return data;
+}
